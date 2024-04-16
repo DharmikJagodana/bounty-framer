2feecb3f2e205176a6bb9efda19477f1bb452970
Dharmik Jagodana <dharmikjagodana007@gmail.com>
2024-04-16 10:13:21 +0530

    added apis to check if follows, recasted, and get connected accounts

--- src/app/frames/route.ts
+++ src/app/frames/route.ts
@@ -1,9 +1,9 @@
 // Route that handles frame actions
 import { prisma } from '@/db';
-import { Prisma } from "@prisma/client"
 import { config } from '@/lib/config';
 import { getFrameHtml, validateFrameMessage, Frame, FrameActionPayload } from "frames.js";
 import { NextRequest } from "next/server";
+import { farcasterService } from '@/service/farcaster';
 
 
 export async function POST(request: NextRequest) {
@@ -15,6 +15,34 @@ export async function POST(request: NextRequest) {
   if (!isValid || !message) {
     return new Response("Invalid message", { status: 400 });
   }
+
+  const userFid = body.untrustedData.fid
+  const targetFid = body.untrustedData.fid
+  const castHash = body.untrustedData.castId.hash
+
+  const promises = []
+  promises.push(farcasterService.checkIfFollows(userFid, targetFid))
+  promises.push(farcasterService.checkIfRecastedFrame(userFid, targetFid, castHash))
+  promises.push(farcasterService.getConnectEthAddress(userFid))
+
+  const [[follows], [recasted], [isConnected, ethAddresses]] = await Promise.all(promises)
+  let connectedAddresses = ethAddresses.split(",")
+  console.log({
+    follows,
+    recasted,
+    isConnected,
+    eth: connectedAddresses
+  });
+
+
+  if (!follows) {
+    return mustFollowFrame(userFid, targetFid)
+  }
+
+  if (!recasted) {
+    return mustRecastFrame(userFid, targetFid)
+  }
+
   // logs request body
   const result = await prisma.requestLogs.create({
     data: {
@@ -64,6 +92,61 @@ export async function POST(request: NextRequest) {
         label: `Next (pressed by ${message.data.fid})`,
         action: "link",
         target: `https://www.google.com/search?q=${randomInt}`,
+      },
+    ],
+    ogImage: `${imageUrlBase}/600`,
+    postUrl: `${config.host}/frames`,
+  };
+
+  // Return the frame as HTML
+  const html = getFrameHtml(frame);
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+    status: 200,
+  });
+}
+
+
+function mustFollowFrame(fid: number, targetFid: number) {
+  const randomInt = Math.floor(Math.random() * 100);
+  const imageUrlBase = `https://picsum.photos/seed/${randomInt}`;
+  // Use the frame message to build the frame
+  const frame: Frame = {
+    version: "vNext",
+    image: `${imageUrlBase}/1146/600`,
+    buttons: [
+      {
+        label: `Follow and Refresh`,
+        action: "post"
+      },
+    ],
+    ogImage: `${imageUrlBase}/600`,
+    postUrl: `${config.host}/frames`,
+  };
+
+  // Return the frame as HTML
+  const html = getFrameHtml(frame);
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+    status: 200,
+  });
+}
+
+function mustRecastFrame(fid: number, targetFid: number) {
+  const randomInt = Math.floor(Math.random() * 100);
+  const imageUrlBase = `https://picsum.photos/seed/${randomInt}`;
+  // Use the frame message to build the frame
+  const frame: Frame = {
+    version: "vNext",
+    image: `${imageUrlBase}/1146/600`,
+    buttons: [
+      {
+        label: `Recast and Refresh`,
+        action: "post"
       },
     ],
     ogImage: `${imageUrlBase}/600`,
---
+++ src/service/farcaster/index.ts
@@ -0,0 +1,128 @@
+import axios from 'axios';
+
+const baseURL = 'https://nemes.farcaster.xyz:2281';
+
+interface FarcasterErrorResponse {
+  errCode: string;
+  presentable: boolean;
+  name: string;
+  code: number;
+  details: string;
+  metadata: Object
+}
+
+interface IVerificationAddBody {
+  address: string;
+  claimSignature: string;
+  blockHash: string;
+  verificationType: number,
+  chainId: number,
+  protocol: string,
+  ethSignature: string,
+}
+interface IVerificationResponseItem {
+  data: {
+    type: string;
+    fid: number;
+    timestamp: number;
+    network: string;
+    verificationAddAddressBody: IVerificationAddBody
+    verificationAddEthAddressBody: IVerificationAddBody
+  }
+  hash: string;
+  hashScheme: string;
+  signature: string;
+  signatureScheme: string;
+  signer: string;
+}
+
+
+async function checkIfFollows(fid: number, targetFid: number):
+  Promise<[boolean, string]> {
+  try {
+    if (fid === targetFid) {
+      return [true, "User follows target"];
+    }
+    const response = await axios.get(`${baseURL}/v1/linkById?fid=${fid}&target_fid=${targetFid}&link_type=follow`);
+    console.log(response.data);
+    return [true, response.data];
+  } catch (e: any) {
+    if (e.response) {
+      const statusCode = e.response.status;
+      if (statusCode === 400) {
+        const data = e.response.data as FarcasterErrorResponse
+        if (data?.errCode == 'not_found') {
+          return [false, data.details];
+        }
+      }
+      console.error(e);
+      return [false, e.response.data];
+    }
+    console.error(e);
+    return [false, e];
+  }
+}
+
+async function checkIfRecastedFrame(
+  fid: number,
+  targetFid: number,
+  targetFrameHash: string,
+): Promise<[boolean, string]> {
+  try {
+    if (fid === targetFid) {
+      return [true, "User has recasted frame"];
+    }
+    const response = await axios.get(`${baseURL}/v1/reactionById?fid=${fid}&reaction_type=1&target_fid=${targetFid}&target_hash=${targetFrameHash}`);
+    console.log(response.data);
+    return [true, response.data];
+  } catch (e: any) {
+    if (e.response) {
+      const statusCode = e.response.status;
+      if (statusCode === 400) {
+        const data = e.response.data as FarcasterErrorResponse
+        if (data?.errCode == 'not_found') {
+          return [false, data.details];
+        }
+      }
+      console.error(e);
+      return [false, e.response.data];
+    }
+    console.error(e);
+    return [false, e];
+  }
+}
+
+async function getConnectEthAddress(fid: number): Promise<[boolean, string]> {
+  try {
+    const response = await axios.get(`${baseURL}/v1/verificationsByFid?fid=${fid}`);
+    const data = response.data as {
+      messages: IVerificationResponseItem[],
+    }
+    console.log("response.data");
+    console.log(response.data);
+
+    const array = data.messages;
+    let addresses: string[] = []
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i];
+      if (item.data.type === 'MESSAGE_TYPE_VERIFICATION_ADD_ETH_ADDRESS') {
+        if (item.data.verificationAddEthAddressBody.protocol == "PROTOCOL_ETHEREUM") {
+          addresses.push(item.data.verificationAddEthAddressBody.address);
+        }
+      }
+    }
+    if (addresses.length === 0) {
+      return [false, "No address found"];
+    }
+    return [true, addresses.toString()];
+  } catch (e: any) {
+    console.error(e);
+    return [false, e];
+  }
+}
+
+export const farcasterService = {
+  checkIfFollows,
+  checkIfRecastedFrame,
+  getConnectEthAddress,
+}
