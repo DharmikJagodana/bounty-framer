1d8cf65628ea8335990aab75eee5b795aadf338f
Dharmik Jagodana <dharmikjagodana007@gmail.com>
2024-04-17 22:00:06 +0530

    Added github info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { assets, config } from '@/lib/config';
-import { Frame, getFrameFlattened } from "frames.js";
+import { Frame, getFrameFlattened } from 'frames.js';
 import Link from 'next/link';
 
 const initialFrame: Frame = {
   image: assets.nft,
-  version: "vNext",
+  version: 'vNext',
   buttons: [
     {
-      label: "Mint NFT",
-      action: "post",
+      label: 'Mint NFT',
+      action: 'post',
     },
   ],
   postUrl: `${config.host}/frames`,
@@ -16,35 +16,69 @@ const initialFrame: Frame = {
 
 export async function generateMetadata() {
   return {
-    title: "Bounty Frames",
+    title: 'Bounty Frames',
     other: getFrameFlattened(initialFrame),
   };
 }
 
 export default function Page() {
-  return <main>
-    <div className='flex h-screen w-screen items-center justify-center'>
-      <div className='text-center flex gap-y-2 flex-col'>
-        <h1 className='text-4xl font-bold'>Bounty Frames</h1>
-        <p>
-          This is sample example to mint NFT using Frames in farcaster.
-        </p>
-        <div>
+  return (
+    <main className="flex flex-col gap-2 h-screen container">
+      <div className="flex flex-1 items-center justify-center">
+        <div className="text-center flex gap-y-4 flex-col">
           <div>
-            Copy url and paste in farcaster to mint NFT
+            <h1 className="text-4xl font-bold">
+              <Link
+                href={
+                  'https://earn.superteam.fun/listings/bounty/create-a-frame-for-farcaster/'
+                }
+              >
+                Bounty Frames
+              </Link>
+            </h1>
+            <p>This is sample example to mint NFT using Frames in farcaster.</p>
           </div>
-          <div className='font-bold text-blue-400'>
-            {config.host}
+          <div>
+            <div>Share following URL in farcaster to mint NFT</div>
+            <div className="font-bold text-blue-400">
+              <Link href={config.host} target="_blank">
+                {config.host}
+              </Link>
+            </div>
           </div>
-        </div>
-        <div>
-          Build by <Link
-            className='text-blue-400 font-bold'
-            href={"https://warpcast.com/dharmikjagodana"}>
-            @dharmikjagodana
-          </Link>
+          <div>
+            Build by{' '}
+            <Link
+              className="text-blue-400 font-bold"
+              target="_blank"
+              href={'https://warpcast.com/dharmikjagodana'}
+            >
+              @dharmikjagodana
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  </main>
+      <div className="pb-8 flex items-center justify-center">
+        <Link
+          className="text-blue-400 font-bold"
+          target="_blank"
+          href={'https://github.com/DharmikJagodana/bounty-framer'}
+        >
+          <div className="text-center flex items-center justify-center flex-col">
+            <div>Source code </div>
+            <div className="fle items-center justify-center">
+              <svg width="98" height="96" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                  fill="#24292f"
+                />
+              </svg>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </main>
+  );
 }
